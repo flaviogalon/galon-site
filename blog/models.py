1521c4ce589926a1d3414f4d41cea3feb98e17d8
Flavio Galon <flaviogalon@gmail.com>
2022-01-03 08:38:07 -0300

    Updating models, registering models on Admin page and creating new migration

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 """Module that provides models to the blog."""
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
@@ -26,10 +27,10 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.TextField()
     image_name = models.CharField(max_length=200)
-    date = models.DateField()
-    slug = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)  # SlugField or unique makes the field indexed
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name="posts")
     tag = models.ManyToManyField(Tag, null=True)
 
     def __str__(self) -> str:
